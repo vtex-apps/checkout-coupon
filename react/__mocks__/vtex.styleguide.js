4df8b58795808d5f47ab764152e43aea39301947
Jefferson <jefiim.ad@gmail.com>
2019-11-06 19:35:50 -0300

    Update tests

--- react/__mocks__/vtex.styleguide.js
+++ react/__mocks__/vtex.styleguide.js
@@ -3,5 +3,6 @@ import Button from '@vtex/styleguide/lib/Button'
 import Input from '@vtex/styleguide/lib/Input'
 import Tag from '@vtex/styleguide/lib/Tag'
 import ButtonPlain from '@vtex/styleguide/lib/ButtonPlain'
+import InputButton from '@vtex/styleguide/lib/InputButton'
 
-module.exports = { Button, ButtonPlain, Input, Tag }
+module.exports = { Button, ButtonPlain, Input, InputButton, Tag }
