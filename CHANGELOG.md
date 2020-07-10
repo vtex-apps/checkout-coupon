c70191ffd921d3bd36c3a7cc7cb16eda300b81a3
Christian Kaisermann <christian@kaisermann.me>
2020-07-10 10:55:16 -0300

    Update tooling pkgs and lint project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Changed
+
+- Project linted and re-formatted.
+
 ## [0.10.1] - 2019-12-05
 
 ### Changed
--- react/components/TranslatedTitle.tsx
+++ react/components/TranslatedTitle.tsx
@@ -3,7 +3,8 @@ import { FormattedMessage } from 'react-intl'
 
 // The b-s token makes the `b` class be applied only on mobile devices.
 const TranslatedTile: FunctionComponent<TranslatedTileProps> = ({ title }) => {
-  const titleText = title || <FormattedMessage id="base.title" />
+  const titleText = title ?? <FormattedMessage id="base.title" />
+
   return (
     <div className="flex flex-grow-1 justify-center b-s">
       <span className="t-heading-4">{titleText}</span>
